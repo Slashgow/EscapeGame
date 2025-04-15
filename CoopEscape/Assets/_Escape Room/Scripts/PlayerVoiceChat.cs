ffa9b9c71c99c0f75b2b613f9e79243c579f9908
William <william.plank@outlook.fr>
2025-04-15 14:17:29 +0200

    receive audio

--- CoopEscape/Assets/_Escape Room/Scripts/PlayerVoiceChat.cs
+++ CoopEscape/Assets/_Escape Room/Scripts/PlayerVoiceChat.cs
@@ -2,11 +2,16 @@ using System.Linq;
 using PurrNet;
 using Steamworks;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
 
 public class PlayerVoiceChat : NetworkBehaviour
 {
     [SerializeField]
     private PlayerInputHandler playerInputHandler;
+
+    [SerializeField]
+    private AudioSource playerAudioSource;
 
     private bool isPushToTalkRecording;
 
@@ -53,23 +58,39 @@ public class PlayerVoiceChat : NetworkBehaviour
                 voiceResult = SteamUser.GetVoice(true, destBuffer, 1024, out uint bytesWritten);
                 if(voiceResult == EVoiceResult.k_EVoiceResultOK &&  bytesWritten > 0)
                 {
-                    SendVoiceToOtherPlayer(otherPlayerID ,bytesWritten);
+                    SendVoiceToOtherPlayer(otherPlayerID, destBuffer ,bytesWritten);
                 }
             }
         }
     }
 
     [ServerRpc]
-    private void SendVoiceToOtherPlayer(PlayerID target, uint bytesWritten, RPCInfo info = default)
+    private void SendVoiceToOtherPlayer(PlayerID target, byte[] data, uint bytesWritten, RPCInfo info = default)
     {
         Debug.Log($"Send voice to other player server : {info.sender}");
-        SendVoiceToOtherPlayer_Target(target, bytesWritten);
+        SendVoiceToOtherPlayer_Target(target, data, bytesWritten);
     }
 
     [TargetRpc]
-    private void SendVoiceToOtherPlayer_Target(PlayerID target, uint bytesWritten, RPCInfo info = default)
+    private void SendVoiceToOtherPlayer_Target(PlayerID target, byte[] data, uint bytesWritten, RPCInfo info = default)
     {
         Debug.Log($"send voice to other player target {target}");
         Debug.Log($"send voice to other player target, Sender: {info.sender}");
+
+        byte[] destBuffer2 = new byte[22050 * 2];
+        uint bytesWritten2;
+        EVoiceResult ret = SteamUser.DecompressVoice(data, bytesWritten, destBuffer2, (uint)destBuffer2.Length, out bytesWritten2, 22050);
+        if (ret == EVoiceResult.k_EVoiceResultOK && bytesWritten2 > 0)
+        {
+            playerAudioSource.clip = AudioClip.Create(UnityEngine.Random.Range(100, 1000000).ToString(), 22050, 1, 22050, false);
+
+            float[] test = new float[22050];
+            for (int i = 0; i < test.Length; i++)
+            {
+                test[i] = (short)(destBuffer2[i * 2] | destBuffer2[i * 2 + 1] << 8) / 32768.0f;
+            }
+            playerAudioSource.clip.SetData(test, 0);
+            playerAudioSource.Play();
+        }
     }
 }
